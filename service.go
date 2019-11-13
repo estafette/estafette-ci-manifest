8afffa988ad306706cdb35b6fb8a4ef6c1d72c3f
Jorrit Salverda <jsalverda@travix.com>
2019-11-13 19:19:01 +0100

    add customproperties to EstafetteService and add UnmarshalYAML for it

--- service.go
+++ service.go
@@ -8,6 +8,7 @@ type EstafetteService struct {
 	Ports              []*EstafetteServicePort `yaml:"ports,omitempty"`
 	Command            string                  `yaml:"command,omitempty"`
 	ContinueAfterStage bool                    `yaml:"continueAfterStage,omitempty"`
+	CustomProperties   map[string]interface{}  `yaml:",inline"`
 }
 
 // EstafetteServicePort represents a port to be opened on the service container and an optional external port if port mapping to a different port is required
@@ -21,4 +22,36 @@ type EstafetteServicePort struct {
 type ReadinessProbe struct {
 	Path           string `yaml:"path,omitempty"`
 	TimeoutSeconds int    `yaml:"timeoutSeconds,omitempty"`
+}
+
+// UnmarshalYAML customizes unmarshalling an EstafetteService
+func (service *EstafetteService) UnmarshalYAML(unmarshal func(interface{}) error) (err error) {
+
+	var aux struct {
+		Name               string                  `yaml:"name,omitempty"`
+		ContainerImage     string                  `yaml:"image,omitempty"`
+		EnvVars            map[string]string       `yaml:"env,omitempty"`
+		Ports              []*EstafetteServicePort `yaml:"ports,omitempty"`
+		Command            string                  `yaml:"command,omitempty"`
+		ContinueAfterStage bool                    `yaml:"continueAfterStage,omitempty"`
+		CustomProperties   map[string]interface{}  `yaml:",inline"`
+	}
+
+	// unmarshal to auxiliary type
+	if err := unmarshal(&aux); err != nil {
+		return err
+	}
+
+	// map auxiliary properties
+	service.Name = aux.Name
+	service.ContainerImage = aux.ContainerImage
+	service.EnvVars = aux.EnvVars
+	service.Ports = aux.Ports
+	service.Command = aux.Command
+	service.ContinueAfterStage = aux.ContinueAfterStage
+
+	// fix for map[interface{}]interface breaking json.marshal - see https://github.com/go-yaml/yaml/issues/139
+	service.CustomProperties = cleanUpStringMap(aux.CustomProperties)
+
+	return nil
 }

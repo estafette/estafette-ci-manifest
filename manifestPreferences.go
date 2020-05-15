c85967c1d1f566eeca923a1a7901763ab902fbab
Jorrit Salverda <jsalverda@travix.com>
2020-05-15 14:49:04 +0200

    add json annotation to manifest preferences

--- manifestPreferences.go
+++ manifestPreferences.go
@@ -2,7 +2,7 @@ package manifest
 
 // EstafetteManifestPreferences is used to configure validation rules for the manifest
 type EstafetteManifestPreferences struct {
-	LabelRegexes                    map[string]string   `yaml:"labelRegexes,omitempty"`
-	BuilderOperatingSystems         []string            `yaml:"builderOperatingSystems,omitempty"`
-	BuilderTracksPerOperatingSystem map[string][]string `yaml:"builderTracksPerOperatingSystem,omitempty"`
+	LabelRegexes                    map[string]string   `yaml:"labelRegexes,omitempty" json:"labelRegexes,omitempty"`
+	BuilderOperatingSystems         []string            `yaml:"builderOperatingSystems,omitempty" json:"builderOperatingSystems,omitempty"`
+	BuilderTracksPerOperatingSystem map[string][]string `yaml:"builderTracksPerOperatingSystem,omitempty" json:"builderTracksPerOperatingSystem,omitempty"`
 }

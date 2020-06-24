982ff42324e9d7b245303d3b731b7713ffac0b86
Jorrit Salverda <jsalverda@travix.com>
2020-06-24 13:32:12 +0200

    support to hide release action from release badge

--- releaseAction.go
+++ releaseAction.go
@@ -2,5 +2,6 @@ package manifest
 
 // EstafetteReleaseAction represents an action on a release target that controls what happens by running the release stage
 type EstafetteReleaseAction struct {
-	Name string `yaml:"name" json:"name"`
+	Name      string `yaml:"name" json:"name"`
+	HideBadge bool   `yaml:"hideBadge,omitempty" json:"hideBadge,omitempty"`
 }

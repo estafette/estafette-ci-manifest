6b62e89e34d276385d186b2160e03166aef0f96c
Jorrit Salverda <jsalverda@travix.com>
2021-01-20 09:50:37 +0100

    drop hugepages medium; use lowercase value for memory

--- storageMedium.go
+++ storageMedium.go
@@ -3,7 +3,6 @@ package manifest
 type StorageMedium string
 
 const (
-	StorageMediumDefault   StorageMedium = ""          // use whatever the default is for the node, assume anything we don't explicitly handle is this
-	StorageMediumMemory    StorageMedium = "Memory"    // use memory (e.g. tmpfs on linux)
-	StorageMediumHugePages StorageMedium = "HugePages" // use hugepages
+	StorageMediumDefault StorageMedium = ""       // use whatever the default is for the node, assume anything we don't explicitly handle is this
+	StorageMediumMemory  StorageMedium = "memory" // use memory (e.g. tmpfs on linux)
 )

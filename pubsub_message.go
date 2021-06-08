2ed468bde7085b0a0f32e243d9cc7d0619b457eb
Jorrit Salverda <jsalverda@travix.com>
2021-06-08 14:23:58 +0200

    add OrderingKey to PubsubMessage

--- pubsub_message.go
+++ pubsub_message.go
@@ -1,9 +1,12 @@
 package manifest
 
+import "time"
+
 // PubsubMessage is a container for a pubsub push message
 type PubsubMessage struct {
-	Attributes  *map[string]string `json:"attributes,omitempty"`
-	Data        string             `json:"data,omitempty"`
-	MessageID   string             `json:"messageId,omitempty"`
-	PublishTime string             `json:"publishTime,omitempty"`
+	Attributes  map[string]string `json:"attributes,omitempty"`
+	Data        string            `json:"data,omitempty"`
+	MessageID   string            `json:"messageId,omitempty"`
+	PublishTime time.Time         `json:"publishTime,omitempty"`
+	OrderingKey string            `json:"orderingKey,omitempty"`
 }
